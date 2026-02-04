978e0c7c1099d80cef528f93cb6d4940074dc48f
Janisha Manaliyil <janishamanaliyil@gmail.com>
2026-02-04 09:36:00 +0400

    Client class and domain class added in core models.py

--- core/models.py
+++ core/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
+from django_tenants.models import TenantMixin, DomainMixin
+
+class Client(TenantMixin):
+    name = models.CharField(max_length=100)
+    created_on = models.DateField(auto_now_add=True)
+
+    # default true, schema will be automatically created and synced when it is saved
+    auto_create_schema = True
+
+class Domain(DomainMixin):
+    pass
 class User(AbstractUser):
     pass
