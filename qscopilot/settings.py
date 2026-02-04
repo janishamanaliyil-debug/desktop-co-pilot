139fd7f19d69bdc7050f6c6e7c48d317c876bbf5
Janisha Manaliyil <janishamanaliyil@gmail.com>
2026-02-04 09:29:22 +0400

    djangotenant middleware added

--- qscopilot/settings.py
+++ qscopilot/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'django_tenants.middleware.main.TenantMainMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
