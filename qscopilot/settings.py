71a1c5f45e0887cc2988d48803ee9dde3083a92a
Janisha Manaliyil <janishamanaliyil@gmail.com>
2026-02-04 09:24:18 +0400

    Database configured with tenantSQL

--- qscopilot/settings.py
+++ qscopilot/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'qscopilot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+    'default' : {
+        'ENGINE' : 'django_tenants.postgresql_backend',
+        'NAME' : 'desktop_qs' ,
+        'USER' : 'postgres',
+        'PASSWORD' : 'qs@#NWS321',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
 
+
+              }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
